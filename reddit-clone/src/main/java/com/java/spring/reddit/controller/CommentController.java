49b32d7592c2f75d5cc6ac6250ec03d72f247827
yogeshmevada <yogesh.mevada@instarem.com>
2021-12-22 17:57:05 +0530

    Module name change.

--- reddit-clone/src/main/java/com/java/spring/reddit/controller/CommentController.java
+++ reddit-clone/src/main/java/com/java/spring/reddit/controller/CommentController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
